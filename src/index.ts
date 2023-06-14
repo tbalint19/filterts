58a8c889c7ddaf469557b5c76eef419a14eaeb1c
tbalint19 <tothbalint719@gmail.com>
2023-06-14 18:39:07 +0200

    initial filters

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+let words = [ "alma", "korte", "barack", "narancs" ]
+
+let filterByB = (arr: string[]): string[] => {
+
+  let result: string[] = []
+
+  let index = 0
+  while (arr[index] !== undefined) {
+    let shouldAdd = arr[index][0] === "b"
+    if (shouldAdd) {
+      result = [ ...result, arr[index] ]
+    }
+    index += 1
+  }
+
+  return result
+}
+
+let result1 = filterByB(words)
+console.log(result1)
+
+let numbers = [ 1, 4, 78, 100 ]
+
+let filterBy10 = (arr: number[]): number[] => {
+
+  let result: number[] = []
+
+  let index = 0
+  while (arr[index] !== undefined) {
+    let shouldAdd = arr[index] > 9
+    if (shouldAdd) {
+      result = [ ...result, arr[index] ]
+    }
+    index += 1
+  }
+
+  return result
+}
+
+let result2 = filterBy10(numbers)
+console.log(result2)
+
+
+type Person = {
+  name: string
+  age: number
+  gender: "male" | "female"
+}
+
+let people: Person[] = [
+  { name: "John", age: 20, gender: "male" },
+  { name: "James", age: 30, gender: "male" },
+  { name: "Jane", age: 40, gender: "female" },
+]
+
+let filterByGender = (arr: Person[]): Person[] => {
+
+  let result: Person[] = []
+
+  let index = 0
+  while (arr[index] !== undefined) {
+    let shouldAdd = arr[index].gender === "female"
+    if (shouldAdd) {
+      result = [ ...result, arr[index] ]
+    }
+    index += 1
+  }
+
+  return result
+}
+
+let result3 = filterByGender(people)
+console.log(result3)
